0d1cc885bd73031e6949ca0f35c51c241e77488a
Shawn Shacterman <shawnshact@cs-it-6999983.local>
2018-07-25 08:29:54 -0700

    quick merge to do prototyping on home repo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from baseline import Baseline
 db_path = str(sys.argv[1])
 storage_folder = str(sys.argv[2])
 BATCH_SIZE = 10
+CHUNK_SIZE = 1000
 
 model = Baseline(db_path, storage_folder, BATCH_SIZE)
 model.train()
